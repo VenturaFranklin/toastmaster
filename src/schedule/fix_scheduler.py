c406d5e893302275c95ca90cd2cacb8c551dbaa6
Franklin Ventura DEV-Laptop <venturafranklin@gmail.com>
2018-06-26 08:25:35 -0700

    Information to fix clubscheduler

---
+++ src/schedule/fix_scheduler.py
@@ -0,0 +1,15 @@
+'''
+Created on Jan 4, 2018
+
+@author: venturf2
+Delete this:
+"C:\Users\venturf2\AppData\Local\VirtualStore\Windows\SysWOW64\420042.tms"
+
+Rename clubscheduler folder
+Reinstall clubsheduler
+Start club scheduler
+Set dummy information
+Save and exit
+Copy data from old folder
+restart clubscheduler
+'''
